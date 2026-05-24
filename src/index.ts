efabf44518d66e59f09c5be8aa7cd5abb2f52be7
Enzo <ferrarienzo3@hotmail.com>
2026-05-23 22:16:02 -0300

    se agrega el index, se soluciona error en tags que me marcó Lisa

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,64 @@
 import minimist from "minimist";
 import { PelisController } from "./controllers";
 
-function parseaParams(argv) {
+function parsearArgumentos(argv: any) {
   const resultado = minimist(argv);
-
   return resultado;
 }
 
 function main() {
-  const params = parseaParams(process.argv.slice(2));
+  const controller = new PelisController();
+  const args = parsearArgumentos(process.argv.slice(2));
 
-  console.log(params);
+  // 1- Add, para agregar una película nueva
+  if (args._[0] === "add") {
+    // Solución para el error que me marcó Lisa si la tag es undefined: validamos que no sea undefined antes de armar el array
+    let tagsAsignados: string[] = [];
+    if (args.tags) {
+      tagsAsignados = Array.isArray(args.tags) ? args.tags : [args.tags];
+    }
+
+    return controller
+      .add({
+        id: args.id,
+        title: args.title,
+        tags: tagsAsignados,
+      })
+      .then((resultado) => {
+        console.log("¿Peli agregada con éxito?:", resultado);
+        return resultado;
+      });
+  }
+
+  // 2- Comando "get" por ID
+  if (args._[0] === "get" && args._[1]) {
+    const idNum = parseInt(args._[1]);
+    return controller.get({ id: idNum }).then((peli) => {
+      console.log(peli);
+      return peli;
+    });
+  }
+
+  // 3- Búsqueda con filtros
+  if (args.title || args.tag) {
+    return controller
+      .get({
+        search: {
+          title: args.title,
+          tag: args.tag,
+        },
+      })
+      .then((pelisFiltradas) => {
+        console.log(pelisFiltradas);
+        return pelisFiltradas;
+      });
+  }
+
+  // 4- En caso de no usar comandos, que devuelva array completo
+  return controller.get().then((todasLasPelis) => {
+    console.log(todasLasPelis);
+    return todasLasPelis;
+  });
 }
 
 main();
